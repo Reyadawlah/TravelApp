29e20a9e5003ef047e02e7969924821cdb581994
Reya Dawlah <reya.dawlah@utdallas.edu>
2025-01-11 22:00:30 -0600

    Initial Commit

---
+++ TravelApp/TravelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppApp.swift
+//  TravelApp
+//
+//  Created by Reya Dawlah on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelAppTests/TravelAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAppTests.swift
+//  TravelAppTests
+//
+//  Created by Reya Dawlah on 1/11/25.
+//
+
+import Testing
+@testable import TravelApp
+
+struct TravelAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
